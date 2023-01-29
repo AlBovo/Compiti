b819cf54097a3eee309b746f082dc23054be09b5
Error404 <alandavide.bovo.stud@ispascalcomandini.it>
2023-01-29 15:44:30 +0100

    Nuovi compiti

---
+++ src/test/kotlin/CalculateMCMKtTest.kt
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.*
+class CalculateMCMKtTest{
+    @Test
+    fun singleValueCannotBeZero(){
+        assertThrows<IllegalArgumentException> {calculateMCM(0, 10)}
+    }
+
+    @Test
+    fun singleValueCannotBeZero2(){
+        assertThrows<IllegalArgumentException> {calculateMCM(10,0)}
+    }
+
+    @Test
+    fun bothValuesCannotBeZero(){
+        assertThrows<IllegalArgumentException> {calculateMCM(0, 0)}
+    }
+
+    @Test
+    fun numberCannotBeLowerThanZero(){
+        assertThrows<IllegalArgumentException> {calculateMCM(-10, 32)}
+    }
+
+    @Test
+    fun numbersMustBeBothPositive(){
+        var expected = 230
+        Assertions.assertEquals(expected, calculateMCM(23, 10))
+    }
+
+    @Test
+    fun numberCannotBeBothZeroAndNegative(){
+        assertThrows<IllegalArgumentException> {calculateMCM(-10, 0)}
+    }
+
+    @Test
+    fun bothNumbersArePrime() {
+        var expected = 35
+        Assertions.assertEquals(expected, calculateMCM(5, 7))
+    }
+}
