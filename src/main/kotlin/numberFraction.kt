60510ba6ac67bd1226900f3a708265ed85bf9a27
AlBovo <88632271+AlBovo@users.noreply.github.com>
2023-02-13 12:31:20 +0100

    popipopi

--- src/main/kotlin/numberFraction.kt
+++ src/main/kotlin/numberFraction.kt
@@ -14,4 +14,5 @@ class NumberFraction(var numerator: Int, denominatorValue: Int){
     fun product(numberSecond: NumberFraction) = NumberFraction(numerator * numberSecond.numerator, denominator * numberSecond.denominator)
     fun sum(numberSecond: NumberFraction) = NumberFraction(numerator*numberSecond.denominator + numberSecond.numerator * denominator, denominator*numberSecond.denominator)
     fun isPositive() = (denominator * numerator >= 0)
+    fun isEqual(numberSecond: NumberFraction) = (numerator == numberSecond.numerator && denominator == numberSecond.denominator)
 }
--- src/test/kotlin/NumberFractionTest.kt
+++ src/test/kotlin/NumberFractionTest.kt
@@ -7,68 +7,70 @@ class NumberFractionTest{
     }
 
     @Test
-    fun canHaveEveryPossibleProduct(){
-        val expected = 55
-        val expectedSecond = 32
+    fun canHavePositiveProduct(){
+        val expected = true
         val number = NumberFraction(11, 8)
         val numberSecond = NumberFraction(5, 4)
         val numberThird = number.product(numberSecond)
-        Assertions.assertEquals(expected, numberThird.numerator)
-        Assertions.assertEquals(expectedSecond, numberThird.denominator)
+        Assertions.assertEquals(expected, numberThird.isEqual(NumberFraction(55, 32)))
     }
 
     @Test
     fun canHaveNegativeNumerator(){
-        val expected = -55
-        val expectedSecond = 32
+        val expected = true
         val number = NumberFraction(-11, 8)
         val numberSecond = NumberFraction(5, 4)
         val numberThird = number.product(numberSecond)
-        Assertions.assertEquals(expected, numberThird.numerator)
-        Assertions.assertEquals(expectedSecond, numberThird.denominator)
+        Assertions.assertEquals(expected, numberThird.isEqual(NumberFraction(-55, 32)))
     }
 
     @Test
     fun canHaveNegativeDenominator(){
-        val expected = 55
-        val expectedSecond = -32
+        val expected = true
         val number = NumberFraction(11, 8)
         val numberSecond = NumberFraction(5, -4)
         val numberThird = number.product(numberSecond)
-        Assertions.assertEquals(expected, numberThird.numerator)
-        Assertions.assertEquals(expectedSecond, numberThird.denominator)
+        Assertions.assertEquals(expected, numberThird.isEqual(NumberFraction(55, -32)))
     }
 
     @Test
     fun canHavePositiveSum(){
-        val expected = 20
-        val expectedSecond = 8
+        val expected = true
         val number = NumberFraction(4, 2)
         val numberSecond = NumberFraction(2, 4)
         val numberThird = number.sum(numberSecond)
-        Assertions.assertEquals(expected, numberThird.numerator)
-        Assertions.assertEquals(expectedSecond, numberThird.denominator)
+        Assertions.assertEquals(expected, numberThird.isEqual(NumberFraction(20, 8)))
     }
 
     @Test
     fun canHaveNegativeSum(){
-        val expected = -20
-        val expectedSecond = 8
+        val expected = true
         val number = NumberFraction(-4, 2)
         val numberSecond = NumberFraction(-2, 4)
         val numberThird = number.sum(numberSecond)
-        Assertions.assertEquals(expected, numberThird.numerator)
-        Assertions.assertEquals(expectedSecond, numberThird.denominator)
+        Assertions.assertEquals(expected, numberThird.isEqual(NumberFraction(-20, 8)))
     }
 
     @Test
     fun canHaveZeroNumerator(){
-        val expected = 0
-        val expectedSecond = 8
+        val expected = true
         val number = NumberFraction(0, 2)
         val numberSecond = NumberFraction(0, 4)
         val numberThird = number.sum(numberSecond)
-        Assertions.assertEquals(expected, numberThird.numerator)
-        Assertions.assertEquals(expectedSecond, numberThird.denominator)
+        Assertions.assertEquals(expected, numberThird.isEqual(NumberFraction(0, 8)))
+    }
+
+    @Test
+    fun canHavePositiveFraction(){
+        val expected = true
+        val number = NumberFraction(3, 1)
+        Assertions.assertEquals(expected, number.isPositive())
+    }
+
+    @Test
+    fun canHaveNegativeFraction(){
+        val expected = false
+        val number = NumberFraction(-4, 1)
+        Assertions.assertEquals(expected, number.isPositive())
     }
 }
