00d276d63ea964b02a6532ce8fef633f90fb0819
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-03-20 11:50:54 +0100

    nuovo compito

--- src/main/kotlin/trafficLight.kt
+++ src/main/kotlin/trafficLight.kt
@@ -1,48 +1,25 @@
 class TrafficLight(val type: Int){
     var status: Int = 0 // 0: Rosso, 1: Giallo, 2: Verde
-        private set(value){
-            when (value) {
-                0 -> {
-                    println("Now the color is red")
-                }
-                1 -> {
-                    println("Now the color is yellow")
-                }
-                2 -> {
-                    println("Now the color is green")
-                }
-            }
-            field = value
-        }
+        private set
     init{
         require(type == 0 || type == 1){
             "The type it's not correct"
         }
     }
-    fun changeStatus(newStatus: Int){
-        require(newStatus != status){
-            "The new status must be different from the actual"
-        }
-        require(newStatus == 0 || newStatus == 2) {
-            "The new status it's not correct"
+    fun changeStatusGreater(){
+        check(status in 0..1){
+            "Status out of range"
         }
         if(type == 0){ // Italiano
-            if(status == 0){
-                status = 2
-            }
-            else{
-                status = 1
-                status = 0
-            }
+            status = 2
         } else {
-            if(status == 0){
-                status = 1
-                status = 2
-            }
-            else{
-                status = 1
-                status = 0
-            }
+            status++
         }
+    }
+    fun changeStatusLess(){
+        check(status in 1..2){
+            "Status out of range"
+        }
+        status--
     }
 }
--- src/test/kotlin/TrafficLightTest.kt
+++ src/test/kotlin/TrafficLightTest.kt
@@ -1,39 +1,99 @@
 import org.junit.jupiter.api.*
+
 internal class TrafficLightTest{
     @Test
-    fun typeCanBeOnlyItalian(){
+    fun canMoveFromRedToGreenItalian(){
         val traffic = TrafficLight(0)
-        val expected = 0
-        Assertions.assertEquals(expected, traffic.type)
+        val expected = 2
+        traffic.changeStatusGreater()
+        Assertions.assertEquals(expected, traffic.status)
     }
     @Test
-    fun typeCanBeOnlyInternational(){
-        val traffic = TrafficLight(1)
+    fun canMoveFromGreenToYellowItalian(){
+        val trafficLight = TrafficLight(0)
         val expected = 1
-        Assertions.assertEquals(expected, traffic.type)
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusLess()
+        Assertions.assertEquals(expected, trafficLight.status)
     }
     @Test
-    fun cannotHaveNewStatusSameAsActual(){
-        val traffic = TrafficLight(0)
-        assertThrows<IllegalArgumentException> { traffic.changeStatus(0) }
+    fun canMoveFromYellowToRedItalian(){
+        val trafficLight = TrafficLight(0)
+        val expected = 0
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusLess()
+        trafficLight.changeStatusLess()
+        Assertions.assertEquals(expected, trafficLight.status)
     }
     @Test
-    fun cannotHaveNegativeStatus(){
-        val traffic = TrafficLight(0)
-        assertThrows<IllegalArgumentException> { traffic.changeStatus(-1) }
+    fun cannotMoveToGreaterFromGreenItalian(){
+        val trafficLight = TrafficLight(0)
+        trafficLight.changeStatusGreater()
+        assertThrows<IllegalStateException> { trafficLight.changeStatusGreater() }
     }
     @Test
-    fun cannotHaveStatusGreaterThanTwo(){
-        val traffic = TrafficLight(0)
-        assertThrows<IllegalArgumentException> { traffic.changeStatus(3) }
+    fun cannotMoveToLessFromRedItalian(){
+        val trafficLight = TrafficLight(0)
+        assertThrows<IllegalStateException> { trafficLight.changeStatusLess() }
     }
     @Test
-    fun cannotHaveNewStatusOne(){
-        val traffic = TrafficLight(0)
-        assertThrows<IllegalArgumentException> { traffic.changeStatus(1) }
+    fun canMoveFromRedToYellowInternational(){
+        val trafficLight = TrafficLight(1)
+        val expected = 1
+        trafficLight.changeStatusGreater()
+        Assertions.assertEquals(expected, trafficLight.status)
     }
     @Test
-    fun test(){
-        
+    fun canMoveFromYellowToGreenInternational(){
+        val trafficLight = TrafficLight(1)
+        val expected = 2
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusGreater()
+        Assertions.assertEquals(expected, trafficLight.status)
+    }
+    @Test
+    fun canMoveFromGreenToYellowInternational(){
+        val trafficLight = TrafficLight(1)
+        val expected = 1
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusLess()
+        Assertions.assertEquals(expected, trafficLight.status)
+    }
+    @Test
+    fun canMoveFromYellowToRedInternational(){
+        val trafficLight = TrafficLight(1)
+        val expected = 0
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusLess()
+        trafficLight.changeStatusLess()
+        Assertions.assertEquals(expected, trafficLight.status)
+    }
+    @Test
+    fun cannotMoveToLessFromRedInternational(){
+        val trafficLight = TrafficLight(1)
+        assertThrows<IllegalStateException> { trafficLight.changeStatusLess() }
+    }
+    @Test
+    fun cannotMoveToGreaterFromGreenInternational(){
+        val trafficLight = TrafficLight(1)
+        trafficLight.changeStatusGreater()
+        trafficLight.changeStatusGreater()
+        assertThrows<IllegalStateException> { trafficLight.changeStatusGreater() }
+    }
+    @Test
+    fun trafficLightTypeCanBeZero(){
+        val trafficLight = TrafficLight(0)
+        val expected = 0
+        Assertions.assertEquals(trafficLight.type, expected)
+    }
+    @Test
+    fun trafficLightTypeCannotBeNegative(){
+        assertThrows<IllegalArgumentException> { val trafficLight = TrafficLight(-1) }
+    }
+    @Test
+    fun trafficLightTypeCannotBeGreaterThanOne(){
+        assertThrows<IllegalArgumentException> { val trafficLight = TrafficLight(2) }
     }
 }
