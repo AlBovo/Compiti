0fc3834712bf3ac27dec17087e922015641d8017
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-03-21 12:36:08 +0100

    update

---
+++ src/main/kotlin/bingoClass.kt
@@ -0,0 +1,26 @@
+import kotlin.random.Random
+class Bingo{
+    private var numbersExtracted = Array(91) { false }
+    fun extractNumber(): Int{
+        require(!isEmpty()){
+            "The bag cannot be empty"
+        }
+        var number = Random.nextInt(1, 91)
+        while(numbersExtracted[number]){
+            number = Random.nextInt(1, 91)
+        }
+        check(!numbersExtracted[number]){
+            "The number extracted must not be already extracted"
+        }
+        println("The number: $number has been extracted")
+        numbersExtracted[number] = true
+        return number
+    }
+    fun isEmpty(): Boolean{
+        var test = true
+        for(i in 1..90){
+            test = test and numbersExtracted[i]
+        }
+        return test
+    }
+}
---
+++ src/test/kotlin/BingoTest.kt
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.*
+class BingoTest{
+    @Test
+    fun atTheStartTheBagMustNotBeEmpty(){
+        val bingo = Bingo()
+        val expected = false
+        Assertions.assertEquals(expected, bingo.isEmpty())
+    }
+    @Test
+    fun cannotExtractTwoTimesTheSameNumber(){
+        val bingo = Bingo()
+        val expected = true
+        for(i in 1..90){
+            var temp = bingo.extractNumber()
+        }
+        Assertions.assertEquals(expected, bingo.isEmpty())
+    }
+    @Test
+    fun cannotExtract91Numbers(){
+        val bingo = Bingo()
+        val expected = true
+        for(i in 1..91){
+            if(i == 91){
+                assertThrows<IllegalArgumentException> { var temp = bingo.extractNumber() }
+            }
+            else {
+                var temp = bingo.extractNumber()
+            }
+        }
+    }
+}
