741993338fc888dfe098ac2d29f63a92f4dd764b
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-10-19 22:33:56 +0200

    esercizi di informatica

---
+++ Informatica/2023-10-18/Esercizi/Esercizio2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Esercizio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dichiarazione delle variabili
+            int left = 1, right = 100, mid; // [left, right)
+            char input;
+            #endregion
+
+            #region Stampa delle regole di gioco
+            Console.WriteLine("Questo programma cercherà di indovinare un numero nel range da 1 a 100");
+            Console.WriteLine("Ogni volta che il programma proverà un numero dire se è maggiore (>), minore(<) o uguale(=)");
+            Console.WriteLine("");
+            #endregion
+
+            #region Gioco con controllo e binary search
+            do
+            {
+                mid = left + (right - left) / 2;
+
+                #region Lettura e controllo dell'input
+                do
+                {
+                    Console.Write($"Il programma ha provato il numero {mid}, inserisci il risultato: ");
+                    input = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+                    if(!(input == '>' || input == '<' || input == '='))
+                    {
+                        Console.WriteLine("L'input inserito non è valido, riprova ... ");
+                    }
+                } while (!(input == '>' || input == '<' || input == '='));
+                #endregion
+
+                #region Caso in cui il giocatore stia cercando di barare
+                if (input != '=' && left > right)
+                {
+                    Console.WriteLine($"L'input inserito non può essere valido poichè il range è composto da un solo elemento ({left})...");
+                    break;
+                }
+                #endregion
+
+                #region Modifica del range della binary search
+                if (input == '>')
+                {
+                    left = mid + 1;
+                }
+                else if(input == '<')
+                {
+                    right = mid - 1;
+                }
+                #endregion
+            } while (input != '=');
+            Console.WriteLine("Arrivederci giocatore ...");
+            #endregion
+
+            Console.Write("Premi un tasto per terminare l'esecuzione del programma ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Informatica/2023-10-18/Esercizi/Esercizio3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Esercizio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dichiarazione delle variabili
+            Random rand = new Random();
+            int input = -1, numeroVincente = rand.Next(1, 101);
+            string inputStr;
+            bool inputOK;
+            #endregion
+
+            #region Gioco con interazione con l'utente
+            do
+            {
+                #region Stampa del risultato dei tentativi
+                if (input != -1) Console.WriteLine($"Il numero inserito è {((input > numeroVincente) ? '>' : '<')}");
+                #endregion
+
+                #region Lettura e parse dell'input
+                do
+                {
+                    Console.Write("Inserisci il numero che vuoi provare a indovinare: ");
+                    inputStr = Console.ReadLine();
+                    inputOK = int.TryParse(inputStr, out input);
+
+                    if (!inputOK)
+                    {
+                        Console.WriteLine("L'input inserito non è un numero intero, riprova ... ");
+                    }
+                    else if(input < 1 || input > 100)
+                    {
+                        Console.WriteLine("Il numero inserito è fuori dal range da 1 a 100, riprova ...");
+                        inputOK = false;
+                    }
+                } while (!inputOK);
+                #endregion
+            } while (input != numeroVincente);
+            Console.WriteLine("Hai indovinato il numero complimenti !");
+            #endregion
+
+            Console.Write("Premi un tasto per terminare l'esecuzione del programma ...");
+            Console.ReadKey();
+        }
+    }
+}
