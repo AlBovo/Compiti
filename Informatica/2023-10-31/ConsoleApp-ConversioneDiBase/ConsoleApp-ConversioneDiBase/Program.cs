6d102391eb2a3a5106aeb7555e29ab42e7145aed
Alan Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-10-31 12:42:09 +0100

    giochino e compito

---
+++ Informatica/2023-10-31/ConsoleApp-ConversioneDiBase/ConsoleApp-ConversioneDiBase/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace ConsoleApp_ConversioneDiBase
+{
+    internal class Program
+    {
+        public const string ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // alfabeto utilizzato
+        
+        #region Metodo per convertire da base X a decimale
+        static int BaseToInt(string input, int baseInput)
+        {
+            int length = input.Length, pos, risultato = 0;
+
+            if (baseInput < 2 || baseInput > 36) // la base non è valida
+            {
+                Console.WriteLine("La base inserita non è valida, riprova ...");
+                return -1;
+            }
+
+            if (length == 0) // la stringa è vuota quindi non valida
+            {
+                Console.WriteLine("La stringa di input è vuota, riprova ...");
+                return -1;
+            }
+
+            #region Conversione cercando i valori nell'alfabeto
+            for (int i = 0; i < length; i++)
+            {
+                pos = ALPHABET.IndexOf(input[i]);
+
+                if (pos == -1) // la stringa da convertire non è base valida
+                {
+                    Console.WriteLine($"L'input inserito non è in base {baseInput}, riprova ...");
+                    return -1;
+                }
+
+                risultato += pos * (int)Math.Pow(baseInput, length - i - 1);
+            }
+            #endregion
+
+            return risultato;
+        }
+        #endregion
+
+        #region Metodo per convertire da decimale a base X
+        static string IntToBase(int input, int baseInput)
+        {
+            int resto;
+            string valore = "";
+
+            if (baseInput < 2 || baseInput > 36) // base non valida
+            {
+                Console.WriteLine("La base inserita non è valida, riprova ...");
+                return "";
+            }
+
+            if (input < 0) // input non valido
+            {
+                Console.WriteLine("L'input inserito non può essere negativo, riprova ...");
+                return "";
+            }
+
+            #region Conversione con il divisioni successive
+            while (input > 0)
+            {
+                resto = input % baseInput;
+                input /= baseInput;
+                valore = ALPHABET[resto] + valore;
+            }
+            #endregion
+
+            return valore;
+        }
+        #endregion
+
+        #region Metodo per leggere un intero
+        static int readInt(string message)
+        {
+            bool inputOK;
+            int input;
+            string inputStr;
+
+            do
+            {
+                Console.Write(message);
+                inputStr = Console.ReadLine();
+                inputOK = int.TryParse(inputStr, out input);
+
+                if (!inputOK)
+                {
+                    Console.WriteLine("L'input inserito non è valido, riprova ...");
+                }
+            } while (!inputOK);
+            return input;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            char choice;
+
+            #region Ciclo per chiedere l'input all'utente
+            do
+            {
+                #region Richiesta dell'azione da eseguire
+                Console.Write("Vuoi convertire ad una base o a un intero? (b/i/f per finire): ");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                #endregion
+
+                switch (choice)
+                {
+                    #region Conversione da decimale a base X
+                    case 'b':
+                        int value = readInt("Inserisci il valore decimale: ");
+                        int baseInput = readInt("Inserisci la base del valore: ");
+                        Console.WriteLine($"Il valore convertito a base {baseInput} è {IntToBase(value, baseInput)}");
+                        break;
+                    #endregion
+
+                    #region Conversione da base X a decimale
+                    case 'i':
+                        Console.Write("Inserisci la stringa da convertire a base 10: ");
+                        string valueString = Console.ReadLine().ToUpper();
+                        baseInput = readInt("Inserisci la base della stringa: ");
+                        Console.WriteLine($"La stringa in base {baseInput} convertita ad intero equivale a {BaseToInt(valueString, baseInput)}");
+                        break;
+                    #endregion
+
+                    #region Uscita dal programma
+                    case 'f':
+                        break;
+                    #endregion
+
+                    #region L'input inserito non è valido
+                    default:
+                        Console.WriteLine("La scelta non è valida, riprova ...");
+                        break;
+                    #endregion
+                }
+
+            } while (choice != 'f');
+            #endregion
+
+            Console.Write("Premi un tasto per terminare l'esecuzione del programma ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Informatica/2023-10-31/TerminalHacker/TerminalHacker/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace TerminalHacker
+{
+    internal class Program
+    {
+        private static string[] TERMINAL =
+        {
+            "                                                                                               \n",
+            "    DATANET PROC RECORD:  45-3456-W-3452                                  Transnet on/xc-3     \n",
+            "                                                                                               \n",
+            "                                  FEDERAL RESERVE TRANSFER NODE                                \n",
+            "                                     National Headquarters                                     \n",
+            "                                                                                               \n",
+            "   ************************** Remote Systems Network Input Station *************************   \n",
+            "   =========================================================================================   \n",
+            "                                                                                               \n",
+            "        [1] Interbank Funds Transfer   (Code Prog: 485-GWU)                                    \n",
+            "        [2] International Telelink Access   (Code Lim: XRP-262)                                \n",
+            "        [3] Remote Facsimile Send/Receive   (Code Tran: 2LZP-517)                              \n",
+            "        [4] Regional Bank Interconnect   (Security Code: 47-B34)                               \n",
+            "        [5] Update System Parameters   (Entry Auth. Req.)                                      \n",
+            "        [6] Remote Operator Logon/Logoff                                                       \n",
+            "                                                                                               \n",
+            "   =========================================================================================   \n",
+            "                                                                                               \n",
+            "                              [ ] Select Option or ESC to Abort                                \n"
+        };
+
+        static void Main(string[] args)
+        {
+            char[][] copyTerminal = new char[TERMINAL.Length][];
+            bool[][] isPrinted = new bool[TERMINAL.Length][];
+            for (int i = 0; i < TERMINAL.Length; i++)
+            {
+                copyTerminal[i] = TERMINAL[i].ToCharArray();
+                isPrinted[i] = new bool[TERMINAL[i].Length];
+            }
+
+            bool toContinue = true, printChar;
+            Random rand = new Random();
+            Console.CursorVisible = false;
+
+            while (toContinue)
+            {
+                toContinue = false;
+                for (int i = 0; i < copyTerminal.Length; i++)
+                {
+                    for (int j = 0; j < copyTerminal[i].Length; j++)
+                    {
+                        if (copyTerminal[i][j] == ' ' || copyTerminal[i][j] == '\n' || isPrinted[i][j])
+                        {
+                            (int, int) pos = Console.GetCursorPosition();
+                            if (copyTerminal[i][j] == '\n')
+                            {
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.SetCursorPosition(pos.Item1 + 1, pos.Item2);
+                            }
+                            continue;
+                        }
+
+                        printChar = rand.Next(0, 10) == 1;
+
+                        if (printChar)
+                        {
+                            copyTerminal[i][j] = TERMINAL[i][j];
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            isPrinted[i][j] = true;
+                        }
+                        else
+                        {
+                            toContinue = true;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            copyTerminal[i][j] = (char)rand.Next(33, 255);
+                        }
+                        Console.Write(copyTerminal[i][j]);
+                    }
+                }
+                Thread.Sleep(5);
+                Console.SetCursorPosition(0, 0);
+            }
+
+            Console.Beep(3000, 500);
+            Console.CursorVisible = true;
+            Console.SetCursorPosition(31, 18);
+            Console.ReadKey();
+        }
+    }
+}
