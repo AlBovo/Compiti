68a865af4beccf1907019b5ef28db378e211db24
Alan Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-11-21 11:13:32 +0100

    esercizi di informatica

---
+++ Informatica/2023-11-21/Compiti/ContoAllaRovescia/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace ContoAllaRovescia
+{
+    internal class Program
+    {
+        #region Matrice contenente le rappresentazioni dei numeri
+        static string[][] cifre =
+        {
+            new string[] 
+            {
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                "       ▓",
+                "       ▓",
+                "       ▓",
+                "        ",
+                "       ▓",
+                "       ▓",
+                "       ▓",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+                "▓       ",
+                "▓       ",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            { 
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                "       ▓",  
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "▓       ",
+                "▓       ",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "▓       ",
+                "▓       ",
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                "        ",
+                "       ▓",
+                "       ▓",
+                "       ▓",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+        };
+        #endregion
+
+        static void Main(string[] args)
+        {
+            for(int i=0; i<10; i++)
+            {
+                (int, int) position = Console.GetCursorPosition();
+                int line = 0;
+                foreach (string s in cifre[i])
+                {
+                    Console.WriteLine(s);
+                    line++;
+                    Console.SetCursorPosition(position.Item1, line);
+                }
+                Console.SetCursorPosition(position.Item1 + 8 + 2, 0); // posizione + lunghezza + spazio
+                Thread.Sleep(1000);
+            }
+
+            Console.SetCursorPosition(0, 8);
+            Console.Write("Premi un tasto per terminare l'esecuzione del programma ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Informatica/2023-11-21/Compiti/PianofortePC/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace PianofortePC
+{
+    internal class Program
+    {
+        static char[] keyboard = { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'w', 'e', 'r', 't', 'y', 'u' };
+        static int[] sound_freq = { 262, 294, 330, 349, 392, 440, 494, 277, 311, 370, 415, 466 };
+        const int DURATA = 400;
+
+        #region Funzione per leggere un carattere
+        static public char LeggiTasto()
+        {
+            if (!Console.KeyAvailable) // tasto premuto
+                return '\0';
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            return key.KeyChar;
+        }
+        #endregion
+
+        static async void SuonaPianoforte(int position) => Console.Beep(sound_freq[position], DURATA);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Premi un tasto per suonare una nota, 'q' per uscire");
+
+            Thread task = new Thread(() => { });
+            bool first = true; // per runnare la prima volta
+            while (true) // fa schifo ma per colpa delle periferiche e dei driver (ho bisogno di un linguaggio di basso livello)
+            {
+                char tastoLetto;
+                while ('\0' == (tastoLetto = LeggiTasto())) continue;
+
+                int i;
+                for (i = 0; i < keyboard.Length; i++)
+                    if (keyboard[i] == tastoLetto) break;
+                
+                if(i == keyboard.Length) break;
+
+                if (first) first = false;
+                else if (!task.IsAlive) task.Interrupt();
+                task = new Thread(() => SuonaPianoforte(i));
+                task.Start();
+            }
+        }
+    }
+}
