e08e197757c84c9e1353ad4b51c6263a9dfeb540
Alan Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2023-11-28 10:45:47 +0100

    cartelle riorganizzate

---
+++ Informatica/2023-11-28/2023-10-31/ConsoleApp-ConversioneDiBase/ConsoleApp-ConversioneDiBase/Program.cs
@@ -0,0 +1,156 @@
+﻿/**********************************
+* Alan Davide Bovo 3H 2023-10-31 *
+*        Conversione Di Base     *
+*********************************/
+
+using System;
+
+namespace ConsoleApp_ConversioneDiBase
+{
+    internal class Program
+    {
+        public const string ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // alfabeto utilizzato
+        
+        #region Metodo per convertire da base X a decimale
+        static int BaseToInt(string input, int baseInput)
+        {
+            int length = input.Length, pos, risultato = 0;
+
+            if (baseInput < 2 || baseInput > 36) // la base non è valida
+            {
+                Console.WriteLine("La base inserita non è valida, riprova ...");
+                return -1;
+            }
+
+            if (length == 0) // la stringa è vuota quindi non valida
+            {
+                Console.WriteLine("La stringa di input è vuota, riprova ...");
+                return -1;
+            }
+
+            #region Conversione cercando i valori nell'alfabeto
+            for (int i = 0; i < length; i++)
+            {
+                pos = ALPHABET.IndexOf(input[i]);
+
+                if (pos == -1) // la stringa da convertire non è base valida
+                {
+                    Console.WriteLine($"L'input inserito non è in base {baseInput}, riprova ...");
+                    return -1;
+                }
+
+                risultato += pos * (int)Math.Pow(baseInput, length - i - 1); // calcolo la conversione
+            }
+            #endregion
+
+            return risultato;
+        }
+        #endregion
+
+        #region Metodo per convertire da decimale a base X
+        static string IntToBase(int input, int baseInput)
+        {
+            int resto; // variabile per calcolare il resto della divisione
+            string valore = ""; // variabile per il valore convertito
+
+            if (baseInput < 2 || baseInput > 36) // base non valida
+            {
+                Console.WriteLine("La base inserita non è valida, riprova ...");
+                return "";
+            }
+
+            if (input < 0) // input non valido
+            {
+                Console.WriteLine("L'input inserito non può essere negativo, riprova ...");
+                return "";
+            }
+
+            #region Conversione con il divisioni successive
+            while (input > 0)
+            {
+                resto = input % baseInput; // calcolo del resto
+                input /= baseInput; // divisioni successive
+                valore = ALPHABET[resto] + valore; // calcolo del valore nella nuova base
+            }
+            #endregion
+
+            return valore;
+        }
+        #endregion
+
+        #region Metodo per leggere un intero
+        static int readInt(string message)
+        {
+            bool inputOK;
+            int input;
+            string inputStr;
+
+            do
+            {
+                Console.Write(message);
+                inputStr = Console.ReadLine();
+                inputOK = int.TryParse(inputStr, out input);
+
+                if (!inputOK)
+                {
+                    Console.WriteLine("L'input inserito non è valido, riprova ...");
+                }
+            } while (!inputOK);
+            return input;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Alan Davide Bovo 3H 2023-10-31";
+            Console.WriteLine("Alan Davide Bovo 3H 2023-10-31");
+            char choice;
+
+            #region Ciclo per chiedere l'input all'utente
+            do
+            {
+                #region Richiesta dell'azione da eseguire
+                Console.Write("Vuoi convertire ad una base o a un intero? (b/i/f per finire): ");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                #endregion
+
+                switch (choice)
+                {
+                    #region Conversione da decimale a base X
+                    case 'b':
+                        int value = readInt("Inserisci il valore decimale: ");
+                        int baseInput = readInt("Inserisci la base del valore: ");
+                        Console.WriteLine($"Il valore convertito a base {baseInput} è {IntToBase(value, baseInput)}"); // stampa del risultato
+                        break;
+                    #endregion
+
+                    #region Conversione da base X a decimale
+                    case 'i':
+                        Console.Write("Inserisci la stringa da convertire a base 10: ");
+                        string valueString = Console.ReadLine().ToUpper();
+                        baseInput = readInt("Inserisci la base della stringa: ");
+                        Console.WriteLine($"La stringa in base {baseInput} convertita ad intero equivale a {BaseToInt(valueString, baseInput)}"); // stampa del risultato
+                        break;
+                    #endregion
+
+                    #region Uscita dal programma
+                    case 'f':
+                        break;
+                    #endregion
+
+                    #region L'input inserito non è valido
+                    default:
+                        Console.WriteLine("La scelta non è valida, riprova ...");
+                        break;
+                    #endregion
+                }
+
+            } while (choice != 'f');
+            #endregion
+
+            Console.Write("Premi un tasto per terminare l'esecuzione del programma ..."); // uscita dal programma
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Informatica/2023-11-28/2023-11-21/Compiti/ContoAllaRovescia/Program.cs
@@ -0,0 +1,152 @@
+﻿/**********************************
+* Alan Davide Bovo 3H 2023-11-21 *
+*        Conto Alla Rovescia     *
+*********************************/
+
+namespace ContoAllaRovescia
+{
+    internal class Program
+    {
+        #region Matrice contenente le rappresentazioni dei numeri
+        static string[][] cifre =
+        {
+            new string[] 
+            {
+                " ▓▓▓▓▓▓ ", // ZERO
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                "       ▓", // UNO
+                "       ▓",
+                "       ▓",
+                "        ",
+                "       ▓",
+                "       ▓",
+                "       ▓",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // DUE
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+                "▓       ",
+                "▓       ",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // TRE
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            { 
+                "▓      ▓", // QUATTRO
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                "       ▓",  
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // CINQUE
+                "▓       ",
+                "▓       ",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // SEI
+                "▓       ",
+                "▓       ",
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // SETTE
+                "       ▓",
+                "       ▓",
+                "        ",
+                "       ▓",
+                "       ▓",
+                "       ▓",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // OTTO
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+
+            new string[]
+            {
+                " ▓▓▓▓▓▓ ", // NOVE
+                "▓      ▓",
+                "▓      ▓",
+                " ▓▓▓▓▓▓ ",
+                "       ▓",
+                "       ▓",
+                " ▓▓▓▓▓▓ ",
+            },
+        };
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Alan Davide Bovo 3H 2023-11-21";
+
+            #region Conto alla rovescia
+            for (int i=0; i<10; i++)
+            {
+                (int, int) position = Console.GetCursorPosition();
+                int line = 0;
+                
+                foreach (string s in cifre[i])
+                {
+                    Console.WriteLine(s);
+                    line++;
+                    Console.SetCursorPosition(position.Item1, line); // traslo a destra il cursore
+                }
+
+                Console.SetCursorPosition(position.Item1 + 8 + 2, 0); // posizione + lunghezza + spazio
+                Thread.Sleep(1000); // wait di 1 secondo
+            }
+            #endregion
+
+            Console.SetCursorPosition(0, 8);
+            Console.Write("Premi un tasto per terminare l'esecuzione del programma ...");
+            Console.ReadKey();
+        }
+    }
+}
