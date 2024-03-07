41664d7d34a52dba7d59708ddfd109fd6ccc8d59
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-03-07 21:21:42 +0100

    Added some homework about reading files in C#

---
+++ Informatica/Teoria/2024-03-07/EserciziFiles/EsercizioPrimo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EserciziFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathRead = @"..\..\..\dati.txt";
+            string pathWrite = @"..\..\..\result.txt";
+            int sum = 0, n = 0;
+            using (StreamReader sr = new StreamReader(pathRead))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sum += int.Parse(line);
+                    n++;
+                }
+                sr.Close();
+            }
+            Console.WriteLine($"The sum of the values is {sum}, and the average is {sum / n}");
+            StreamWriter sw = new StreamWriter(pathWrite);
+            using (StreamReader sr = new StreamReader(pathRead))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (int.Parse(line) * n < sum)
+                        sw.WriteLine(line);
+                }
+                sr.Close();
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ Informatica/Teoria/2024-03-07/EserciziFiles/EsercizioTerzo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EsercizioTerzo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathRead = @"..\..\..\dati.txt";
+            string pathWrite = @"..\..\..\result.txt";
+            List<int> nums = new List<int>();
+            using (StreamReader sr = new StreamReader(pathRead))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    nums.Add(int.Parse(line));
+                sr.Close();
+            }
+            nums.Sort();
+            using (StreamWriter sw = new StreamWriter(pathWrite))
+            {
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    sw.WriteLine(nums[i]);
+                }
+                sw.Close();
+            }
+        }
+    }
+}
