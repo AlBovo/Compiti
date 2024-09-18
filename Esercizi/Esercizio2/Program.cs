ecc45d326fbe6509ffd2fe5f948bbf4469426ae7
Alan Davide Bovo <ALAN DAVIDE.BOVO@local.ittcesena.it>
2024-09-18 12:53:37 +0200

    here we go again, added some code

---
+++ Esercizi/Esercizio2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Esercizio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeOnly time;
+
+            while (true)
+            {
+                Console.Write("Inserisci la prima data (HH:MM): ");
+                string time_str = Console.ReadLine();
+                if(time_str.Length != 5)
+                {
+                    Console.WriteLine("La data non è nel formato corretto");
+                    continue;
+                }
+                if (!TimeOnly.TryParse(time_str, out time))
+                    Console.WriteLine("La data non è nel formato corretto");
+                else
+                    break;
+            }
+
+            Console.Write("L'orario indicato è ");
+            if (time.IsBetween(new TimeOnly(16, 00), new TimeOnly(22, 00)))
+                Console.Write("interno");
+            else
+                Console.Write("esterno");
+            Console.WriteLine(" al range [16:00, 22:00]");
+        }
+    }
+}
---
+++ Esercizi/Esercizio3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Esercizio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime[] dates = {
+                new DateTime(2014, 6, 14, 6, 32, 0),
+                new DateTime(2014, 7, 10, 23, 49, 0),
+                new DateTime(2015, 1, 10, 1, 16, 0),
+                new DateTime(2014, 12, 20, 21, 45, 0),
+                new DateTime(2014, 6, 2, 15, 14, 0)
+            };
+
+            int idx_1 = 0, idx_2 = 1;
+            double mini = double.MaxValue;
+            for (int i = 0; i < dates.Length; i++)
+            {
+                for (int j = i + 1; j < dates.Length; j++)
+                {
+                    TimeSpan delta = dates[i] - dates[j];
+
+                    if (double.Abs(delta.TotalSeconds) < mini)
+                    {
+                        idx_1 = i;
+                        idx_2 = j;
+                        mini = double.Abs(delta.TotalSeconds);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Le date più vicine tra loro sono {dates[idx_1].ToShortDateString()} e {dates[idx_2].ToShortDateString()}");
+        }
+    }
+}
