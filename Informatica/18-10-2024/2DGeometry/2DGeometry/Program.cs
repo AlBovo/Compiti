3d285cbb1df38029006a82579f1bc6356f25544b
AlBovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-10-18 16:14:28 +0200

    finished the project about 2ds figures

--- Informatica/18-10-2024/2DGeometry/2DGeometry/Program.cs
+++ Informatica/18-10-2024/2DGeometry/2DGeometry/Program.cs
@@ -39,6 +39,13 @@
 
     class Triangle : Polygon
     {
+        /* Points of the rectangle
+         *  
+         *     a *
+         *      / \
+         *     /   \
+         *  c *-----* b
+         */
         private Point a, b, c;
         private double height, width;
 
@@ -52,11 +59,23 @@
         }
 
         public override double Area() => width * height / 2;
-        public override double Perimeter() => 0;
+        public override double Perimeter()
+        {
+            double side1 = Math.Sqrt(Math.Pow(a.y - b.y, 2) + Math.Pow(b.x - a.x, 2)); // ab
+            double side2 = Math.Sqrt(Math.Pow(a.y - c.y, 2) + Math.Pow(a.x - c.x, 2)); // ac
+            double side3 = Math.Sqrt(Math.Pow(c.y - b.y, 2) + Math.Pow(c.x - b.x, 2)); // bc
+            return side1 + side2 + side3;
+        }
     }
 
     class Rectangle : Polygon
     {
+        /* Points of the rectangle
+         *  
+         *  a *-------* b
+         *    |       |
+         *  d *-------* c
+         */
         private Point a, b, c, d;
         protected double width, height;
 
@@ -64,8 +83,8 @@
         {
             a = new Point(0, 0);
             b = new Point(a.x + width, a.y);
-            c = new Point(a.x + width, a.y + height);
-            d = new Point(a.x, a.y + height);
+            c = new Point(a.x + width, a.y - height);
+            d = new Point(a.x, a.y - height);
             this.width = width;
             this.height = height;
         }
@@ -75,7 +94,13 @@
     }
 
     class Square : Rectangle
-    {
+    {       
+        /* Points of the square
+         *  
+         *  a *---* b
+         *    |   |
+         *  d *---* c
+         */
         public Square(string name, double side) : base(name, side, side) { }
 
         public override double Area() => width * width;
@@ -84,8 +109,60 @@
 
     class Rhombus : Square
     {
-        public Rhombus(string name) : base(name, 0) { }
-        // TODO : finish
+        /* Points of the rhombus
+         * 
+         *      * a
+         *     / \
+         *    /   \
+         * d *     * b
+         *    \   /
+         *     \ /
+         *      * c
+         */
+        private Point a, b, c, d;
+        private double diagonal1, diagonal2;
+
+        public Rhombus(string name, double diagonal1, double diagonal2) : base(name, diagonal1)
+        {
+            this.diagonal1 = Math.Max(diagonal1, diagonal2);
+            this.diagonal2 = Math.Min(diagonal1, diagonal2);
+
+            a = new Point(0, 0);
+            b = new Point(this.diagonal2 / 2, -this.diagonal1 / 2);
+            c = new Point(0, -this.diagonal1);
+            d = new Point(-this.diagonal2 / 2, -this.diagonal1 / 2);
+        }
+
+        public override double Area() => base.Area() / 2;
+        public override double Perimeter() => Math.Sqrt(Math.Pow(diagonal1 / 2, 2) + Math.Pow(diagonal2 / 2, 2)) * 4;
+    }
+
+    class Trapeze : Polygon
+    {
+        /* Points of the trapeze
+         * 
+         *     a *-------* b
+         *      /         \
+         *     /           \
+         *  d *-------------* c
+         */
+
+        private Point a, b, c, d;
+        private double width1, width2, height;
+
+        public Trapeze(string name, double width1, double width2, double height) : base(name)
+        {
+            this.width1 = Math.Max(width1, width2);
+            this.width2 = Math.Min(width1, width2);
+            
+            a = new Point(0, 0);
+            b = new Point(width2, 0);
+            c = new Point(width2 + (width1 - width2) / 2, -height);
+            d = new Point(-(width1 - width2) / 2, -height);
+        }
+
+        public override double Area() => (width1 + width2) * height / 2;
+        public override double Perimeter() => width1 + width2 + Math.Sqrt(height * height + Math.Pow((width1 - width2) / 2, 2)) * 2;
     }
 
     internal class Program
