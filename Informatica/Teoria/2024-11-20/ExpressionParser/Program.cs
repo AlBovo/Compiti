c0fb526535001ed07af6d50506bd1551e0359987
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-11-20 12:58:13 +0100

    added theory project about parsers

---
+++ Informatica/Teoria/2024-11-20/ExpressionParser/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ExpressionParser
+{
+    internal class Program
+    {
+        static void TestTokenizer(string expr)
+        {
+            Console.Write($"{expr} -->> ");
+
+            try
+            {
+                foreach (Token tok in Token.TokenizeString(expr))
+                {
+                    Console.Write($"{tok.ToString()} ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write($"Exception '{e.Message}'");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            TestTokenizer("3+7*2");
+            TestTokenizer("   3   + 7*   2");
+            TestTokenizer("(3+7)*2");
+
+            TestTokenizer("3.14");
+            TestTokenizer(".14");
+            TestTokenizer("3.14E+3");
+
+            TestTokenizer("4 * / - a 123");
+        }
+    }
+}
