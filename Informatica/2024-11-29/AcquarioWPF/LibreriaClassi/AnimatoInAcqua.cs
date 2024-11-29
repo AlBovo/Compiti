12d13537fa9227a780ffe4bdd40b324d474876e2
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-11-29 10:42:19 +0100

    divided functions in library of classes in AcquarioWPF

---
+++ Informatica/2024-11-29/AcquarioWPF/LibreriaClassi/AnimatoInAcqua.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace LibreriaClassi
+{
+    internal class AnimatoInAcqua : Inanimato
+    {
+        public void Move() { }
+
+        public AnimatoInAcqua(Uri uri, Thickness thickness)
+            : base(uri, thickness) { }
+    }
+}
---
+++ Informatica/2024-11-29/AcquarioWPF/LibreriaClassi/AnimatoSulFondo.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace LibreriaClassi
+{
+    internal class AnimatoSulFondo : AnimatoSulPosto
+    {
+        public AnimatoSulFondo(Uri uri, Thickness thickness, double screenHeight)
+            : base(uri, thickness)
+        {
+            thickness.Top = screenHeight - image.Height;
+            image.Margin = thickness;
+        }
+    }
+}
