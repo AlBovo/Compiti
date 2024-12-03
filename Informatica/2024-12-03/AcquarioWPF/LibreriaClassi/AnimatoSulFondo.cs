888e8d21ce9ffac8d4fdade3ab24bba0439bc29e
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-12-03 11:47:51 +0100

    updated the acquarium's project

---
+++ Informatica/2024-12-03/AcquarioWPF/LibreriaClassi/AnimatoSulFondo.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace LibreriaClassi
+{
+    /// <summary>
+    /// Class to manage a fish on the floor of the acquarium
+    /// </summary>
+    internal class AnimatoSulFondo : AnimatoInAcqua
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="uri">The uri of the image of the fish</param>
+        /// <param name="thickness">The thickness for the wpf</param>
+        /// <param name="screenHeight">The height of the screen</param>
+        public AnimatoSulFondo(Uri uri, Thickness thickness, double screenHeight)
+            : base(uri, thickness)
+        {
+            thickness.Top = screenHeight - image.Height;
+            image.Margin = thickness;
+        }
+    }
+}
