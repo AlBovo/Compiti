8f106e8c8542ebef6d6ff5bc3c4ec6ff731c0351
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-12-19 10:59:42 +0100

    updated the missle class

---
+++ Informatica/2024-12-19/AcquarioWPF/LibreriaClassi/AnimatoSilurante.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LibreriaClassi
+{
+    public class AnimatoSilurante : AnimatoPilotato
+    {
+        private readonly Uri arpione = new("immagini/arpione.png", UriKind.RelativeOrAbsolute);
+        private readonly Canvas canvas;
+
+        protected class Siluro : AnimatoInAcqua
+        {
+            private readonly Canvas canvas;
+
+            public bool Muovi()
+            {
+                if (IsBorder())
+                    return false;
+                Muovi(velocity, size, false);
+                return true;
+            }
+
+            public Siluro(Uri uri, (double, double) thickness, int velocity, Canvas canvas, bool direzione)
+                : base(uri, thickness, velocity, canvas)
+            {
+                DirezioneSinistra = direzione;
+                if (!direzione)
+                    SpecchiaImmagine();
+                this.canvas = canvas;
+            }
+
+            ~Siluro() => canvas.Children.Remove(image);
+        }
+
+        private List<Siluro> siluri = new List<Siluro>();
+
+        private void Spara()
+        {
+            siluri.Add(new(arpione, position, velocity, canvas, DirezioneSinistra));
+        }
+
+        public void MuoviSiluri(object? sender, EventArgs e)
+        {
+            for (int i = 0; i < siluri.Count; i++)
+                if (!siluri[i].Muovi())
+                    siluri.Remove(siluri[i]);
+        }
+
+        public override void Muovi(object? sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    DirezioneAlto = true;
+                    Muovi(velocity, size, true);
+                    break;
+                case Key.Down:
+                    DirezioneAlto = false;
+                    Muovi(velocity, size, true);
+                    break;
+                case Key.Left:
+                    if (!DirezioneSinistra)
+                        SpecchiaImmagine();
+                    DirezioneSinistra = true;
+                    Muovi(velocity, size, false);
+                    break;
+                case Key.Right:
+                    if (DirezioneSinistra)
+                        SpecchiaImmagine();
+                    DirezioneSinistra = false;
+                    Muovi(velocity, size, false);
+                    break;
+                case Key.Space:
+                    Spara();
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        public AnimatoSilurante(Uri uri, (double, double) thickness, int velocity, Canvas canvas)
+            : base(uri, thickness, velocity, canvas) { this.canvas = canvas; }
+    }
+}
---
+++ Informatica/2024-12-19/AcquarioWPF/LibreriaClassi/Inanimato.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LibreriaClassi
+{
+    public class Inanimato
+    {
+        public Image image { get; protected set; }
+        public (double, double) size { get; protected set; }
+        protected (double, double) position = (0, 0);
+        protected int velocity;
+
+        public void AddTransform(Transform t)
+        {
+            TransformGroup transformGroup = new();
+
+            transformGroup.Children.Add(t);
+            transformGroup.Children.Add(image.RenderTransform);
+
+            image.RenderTransform = transformGroup;
+        }
+
+        public bool IsBorder() => 
+            position.Item1 - velocity == 0 || position.Item2 - velocity == 0 || 
+            position.Item1 + velocity == size.Item1 || position.Item2 + velocity == size.Item2;
+
+
+        public Inanimato(Uri uri, (double, double) thickness, int velocity, Canvas canvas)
+        {
+            image = new Image();
+            image.Source = new BitmapImage(uri);
+
+            if (thickness.Item1 <= 0 || thickness.Item1 >= canvas.Width)
+                throw new ArgumentException("Invalid X position");
+            if (thickness.Item2 <= 0 || thickness.Item2 >= canvas.Height)
+                throw new ArgumentException("Invalid Y position");
+
+            position = thickness;
+            AddTransform(new TranslateTransform(thickness.Item1, thickness.Item2));            
+
+            image.RenderTransformOrigin = new Point(0.5, 0.5);
+            canvas.Children.Add(image);
+            size = (canvas.Width, canvas.Height);
+        }
+    }
+}
